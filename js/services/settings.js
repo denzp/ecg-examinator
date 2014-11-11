c540be393cd2f3536add8d9af4703e6d791ca487
user <denis.zaryaev@gmail.com>
2014-11-11 12:31:08 +0200

    QRS wave

--- js/services/settings.js
+++ js/services/settings.js
@@ -10,6 +10,17 @@ angular
   lod: 400
 })
 
-.value('Presets', {
-  // TODO
+.factory('Presets', function() {
+  var normal = {
+    p:   { duration: 0.08,   amplitude: 0.15,  interval: 0.15,  visible: true,  positive: true  },
+    q:   { duration: 0.066,  amplitude: 0.025, interval: 0.166, visible: true,  positive: false },
+    qrs: { duration: 0.08,   amplitude: 1,                      visible: true,  positive: true  },
+    s:   { duration: 0.066,  amplitude: 0.25,  interval: 0.09,  visible: true,  positive: false },
+    t:   { duration: 0.18,   amplitude: 0.3,   interval: 0.2,   visible: true,  positive: true  },
+    u:   { duration: 0.0476, amplitude: 0.035, interval: 0.433, visible: false, positive: false }
+  };
+
+  return {
+    normal: normal
+  };
 });
--- js/services/waves.js
+++ js/services/waves.js
@@ -12,56 +12,67 @@ function waveStub(length) {
 angular
 .module('ecg.services')
 .factory('PWave', function(Settings, Presets) {
-  return function() {
+  return function(preset) {
     // TODO
     return waveStub(Settings.lod);
   }
 })
 
 .factory('QRSWave', function(Settings, Presets) {
-  return function() {
-    // TODO
-    return waveStub(Settings.lod);
+  return function(preset) {
+    var buf = new Array(Settings.lod);
+
+    var li = 30 / Settings.heartRate;
+    var b = (2 * li) / Presets[preset].qrs.duration;
+    var signal = Presets[preset].qrs.positive ? 1 : -1;
+
+    // Each value from x matrix is calculated using the formula bellow, resulting in a 1x200 matrix
+    for(var i = 0; i < buf.length; i++) {
+        buf[i] = /*buf[i] +*/ (((2 * b * Presets[preset].qrs.amplitude) / (i * i * Math.PI * Math.PI)) *
+                (1 - Math.cos((i * Math.PI) / b))) * Math.cos((i * Math.PI) / li);
+    }
+
+    return buf;
   }
 })
 
 .factory('QWave', function(Settings, Presets) {
-  return function() {
+  return function(preset) {
     // TODO
     return waveStub(Settings.lod);
   }
 })
 
 .factory('SWave', function(Settings, Presets) {
-  return function() {
+  return function(preset) {
     // TODO
     return waveStub(Settings.lod);
   }
 })
 
 .factory('TWave', function(Settings, Presets) {
-  return function() {
+  return function(preset) {
     // TODO
     return waveStub(Settings.lod);
   }
 })
 
 .factory('UWave', function(Settings, Presets) {
-  return function() {
+  return function(preset) {
     // TODO
     return waveStub(Settings.lod);
   }
 })
 
 .factory('ResultWave', function(PWave, QRSWave, QWave, SWave, TWave, UWave) {
-  return function() {
+  return function(preset) {
     var components = [
-      PWave(),
-      QRSWave(),
-      QWave(),
-      SWave(),
-      TWave(),
-      UWave()
+      //PWave(preset),
+      QRSWave(preset),
+      //QWave(preset),
+      //SWave(preset),
+      //TWave(preset),
+      //UWave(preset)
     ];
 
     var result = new Array(components[0].length);
@@ -80,7 +91,7 @@ angular
 })
 
 .factory('ResultWaveAccessor', function(ResultWave) {
-  var result = ResultWave();
+  var result = ResultWave('normal');
 
   return function(offset) {
     return result[offset % result.length];
