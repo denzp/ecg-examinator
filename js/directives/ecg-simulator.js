60649ee1d2f3dbbf1d7c376e895e05620df70a8e
Denys Zariaiev <denys.zariaiev@gmail.com>
2014-12-14 13:06:59 +0200

    Ecg simulator refactoring

--- js/directives/ecg-simulator.js
+++ js/directives/ecg-simulator.js
@@ -1,141 +1,186 @@
 'use strict';
 
+function ECGSimulator(Presets, Settings, ResultWave, context) {
+  this.Presets    = Presets;
+  this.Settings   = Settings;
+  this.ResultWave = ResultWave;
+
+  this.ctx = context;
+}
+
+ECGSimulator.prototype.drawGrid = function(startX, width, height) {
+  var linesPerHeight = Math.floor(height / this.Settings.ppm / 2),
+  midHeight = Math.floor(height / 2) - 0.5;
+
+  this.ctx.lineWidth = 1;
+
+  for(var i = 0; i < linesPerHeight; ++i) {
+    if(i % 5 === 0) {
+      this.ctx.strokeStyle = 'rgb(255, 181, 118)';
+    }
+    else {
+      this.ctx.strokeStyle = 'rgb(255, 227, 199)';
+    }
+
+    this.ctx.beginPath();
+    this.ctx.moveTo(startX - 0.5, midHeight - i * this.Settings.ppm);
+    this.ctx.lineTo(startX + width + 0.5, midHeight - i * this.Settings.ppm);
+    this.ctx.stroke();
+
+    this.ctx.beginPath();
+    this.ctx.moveTo(startX - 0.5, midHeight + i * this.Settings.ppm);
+    this.ctx.lineTo(startX + width + 0.5, midHeight + i * this.Settings.ppm);
+    this.ctx.stroke();
+  }
+
+  var i = Math.floor(startX / this.Settings.ppm),
+  currentX = i * this.Settings.ppm - 0.5;
+
+  while(currentX < startX + width) {
+    if(i % 5 === 0) {
+      this.ctx.strokeStyle = 'rgb(255, 181, 118)';
+    }
+    else {
+      this.ctx.strokeStyle = 'rgb(255, 227, 199)';
+    }
+
+    this.ctx.beginPath();
+    this.ctx.moveTo(currentX, 0);
+    this.ctx.lineTo(currentX, 0);
+    this.ctx.stroke();
+
+    this.ctx.beginPath();
+    this.ctx.moveTo(currentX, 0);
+    this.ctx.lineTo(currentX, height);
+    this.ctx.stroke();
+
+    ++i;
+    currentX += this.Settings.ppm;
+  }
+};
+
+ECGSimulator.prototype.run = function($scope) {
+  var self = this;
+
+  this.clear($scope);
+
+  window.requestAnimationFrame(function handler() {
+    self.draw($scope);
+
+    if(!$scope.shouldStopAnimation) {
+      window.requestAnimationFrame(handler);
+    }
+  });
+};
+
+ECGSimulator.prototype.clear = function($scope) {
+  this.ctx.clearRect(0, 0, $scope.width, $scope.height);
+
+  if($scope.grid) {
+    this.drawGrid(0, $scope.width, $scope.height);
+  }
+
+  this.draw1VHelper($scope);
+  this.drawHeartbeats($scope);
+};
+
+ECGSimulator.prototype.draw1VHelper = function($scope) {
+  this.ctx.lineWidth = 1.5;
+  this.ctx.strokeStyle = 'rgb(0, 0, 0)';
+  this.ctx.beginPath();
+  this.ctx.moveTo(5,  this.translateY($scope.height, 0));
+  this.ctx.lineTo(10, this.translateY($scope.height, 0));
+  this.ctx.lineTo(10, this.translateY($scope.height, 1));
+  this.ctx.lineTo(30, this.translateY($scope.height, 1));
+  this.ctx.lineTo(30, this.translateY($scope.height, 0));
+  this.ctx.lineTo(35, this.translateY($scope.height, 0));
+  this.ctx.stroke();
+  this.ctx.lineWidth = 1;
+};
+
+ECGSimulator.prototype.drawHeartbeats = function($scope) {
+  this.ctx.textBaseline = 'top';
+
+  this.ctx.fillStyle = 'black';
+  this.ctx.font = 'bold 10pt monospace';
+  this.ctx.fillText(this.Presets[$scope.preset || 'normal'].beats, 11.5, 5);
+};
+
+ECGSimulator.prototype.MARGIN = {
+  left: 45,
+  right: 10,
+  top: 10,
+  bottom: 10
+};
+
+ECGSimulator.prototype.draw = function($scope) {
+  var pixelsPerSecond    = this.Settings.paperSpeed * this.Settings.ppm,
+      pixelsPerHeartBeet = 60 * pixelsPerSecond / this.Presets[$scope.preset || 'normal'].beats,
+      stepX = pixelsPerHeartBeet / this.Settings.lod,
+      cleanWidth = stepX * 2 + 75;
+
+  var point = this.getNextPoint($scope, stepX);
+
+  if(point.x >= $scope.width - this.MARGIN.right) {
+    if($scope.static) {
+      $scope.shouldStopAnimation = true;
+      return;
+    }
+
+    $scope.last.x = this.MARGIN.left - 1;
+    point.x = this.MARGIN.left;
+  }
+
+  this.ctx.globalCompositeOperation = 'destination-out';
+  this.ctx.fillStyle = this.ctx.createLinearGradient($scope.last.x, 0, $scope.last.x + cleanWidth, 0);
+  this.ctx.fillStyle.addColorStop(0, 'rgba(255, 255, 255, 1.0)');
+  this.ctx.fillStyle.addColorStop(1, 'rgba(255, 255, 255, 0.0)');
+  this.ctx.fillRect($scope.last.x, 0, cleanWidth, $scope.height);
+
+  this.ctx.globalCompositeOperation = 'destination-over';
+  if($scope.grid) {
+    this.drawGrid($scope.last.x, cleanWidth, $scope.height);
+  }
+
+  this.ctx.globalCompositeOperation = 'source-over';
+
+  this.ctx.lineWidth = 1.5;
+  this.ctx.strokeStyle = 'rgb(0, 0, 0)';
+  this.ctx.beginPath();
+  this.ctx.moveTo($scope.last.x - 0.5, $scope.last.y);
+  this.ctx.lineTo(point.x, point.y);
+  this.ctx.stroke();
+
+  $scope.last = point;
+}
+
+ECGSimulator.prototype.translateY = function(height, y) {
+  var h = (height - this.MARGIN.top - this.MARGIN.bottom) / 2;
+  return height - this.MARGIN.top - h - this.Settings.ppm * 10 * y;
+};
+
+ECGSimulator.prototype.getNextPoint = function($scope, stepX) {
+  var x = $scope.last.x + stepX,
+  y = this.ResultWave($scope.last.offset, $scope.preset || 'normal', $scope.noise);
+
+  return {
+    offset: $scope.last.offset + 1,
+    x: x,
+    y: this.translateY($scope.height, y)
+  };
+};
+
+ECGSimulator.prototype.reset = function($scope) {
+  $scope.last = {
+    offset: 0,
+    x: this.MARGIN.left,
+    y: this.translateY($scope.height, this.ResultWave(0, $scope.preset || 'normal', $scope.noise))
+  };
+};
+
 angular
 .module('ecg.directives')
 .directive('ecgSimulator', function(Presets, Settings, ResultWave) {
-  var margin = {
-    left: 45,
-    right: 10,
-    top: 10,
-    bottom: 10
-  };
-
-  function translateY(height, y) {
-    var h = (height - margin.top - margin.bottom) / 2;
-    return height - margin.top - h - Settings.ppm * 10 * y;
-  }
-
-  function getNextPoint($scope, stepX) {
-    var x = $scope.last.x + stepX,
-        y = ResultWave($scope.last.offset, $scope.preset || 'normal', $scope.noise);
-
-    return {
-      offset: $scope.last.offset + 1,
-      x: x,
-      y: translateY($scope.height, y)
-    };
-  }
-
-  function drawGrid(x, width, height) {
-    var linesPerHeight = Math.floor(height / Settings.ppm / 2),
-        midHeight = Math.floor(height / 2) - 0.5;
-
-    for(var i = 0; i < linesPerHeight; ++i) {
-      if(i % 5 === 0) {
-        this.strokeStyle = 'rgb(255, 181, 118)';
-      }
-      else {
-        this.strokeStyle = 'rgb(255, 227, 199)';
-      }
-
-      this.beginPath();
-      this.moveTo(x, midHeight - i * Settings.ppm);
-      this.lineTo(x + width, midHeight - i * Settings.ppm);
-      this.stroke();
-
-      this.beginPath();
-      this.moveTo(x, midHeight + i * Settings.ppm);
-      this.lineTo(x + width, midHeight + i * Settings.ppm);
-      this.stroke();
-    }
-
-    var i = Math.floor(x / Settings.ppm),
-        currentX = i * Settings.ppm - 0.5;
-
-    while(currentX < x + width) {
-      if(i % 5 === 0) {
-        this.strokeStyle = 'rgb(255, 181, 118)';
-      }
-      else {
-        this.strokeStyle = 'rgb(255, 227, 199)';
-      }
-
-      this.beginPath();
-      this.moveTo(currentX, 0);
-      this.lineTo(currentX, 0);
-      this.stroke();
-
-      this.beginPath();
-      this.moveTo(currentX, 0);
-      this.lineTo(currentX, height);
-      this.stroke();
-
-      ++i;
-      currentX += Settings.ppm;
-    }
-  }
-
-  function drawHandler($scope) {
-    var pixelsPerSecond    = Settings.paperSpeed * Settings.ppm,
-        pixelsPerHeartBeet = 60 * pixelsPerSecond / Presets[$scope.preset || 'normal'].beats,
-        stepX = pixelsPerHeartBeet / Settings.lod,
-        cleanWidth = stepX * 2 + 75;
-
-    var point = getNextPoint($scope, stepX);
-
-    if(point.x >= $scope.width - margin.right) {
-      if($scope.static) {
-        $scope.shouldStopAnimation = true;
-        return;
-      }
-
-      $scope.last.x = margin.left - 1;
-      point.x = margin.left;
-    }
-
-    this.globalCompositeOperation = 'destination-out';
-    this.fillStyle = this.createLinearGradient($scope.last.x, 0, $scope.last.x + cleanWidth, 0);
-    this.fillStyle.addColorStop(0, 'rgba(255, 255, 255, 1.0)');
-    this.fillStyle.addColorStop(1, 'rgba(255, 255, 255, 0.0)');
-    this.fillRect($scope.last.x, 0, cleanWidth, $scope.height);
-
-    this.globalCompositeOperation = 'destination-over';
-    if($scope.grid) {
-      drawGrid.call(this, $scope.last.x, cleanWidth, $scope.height);
-    }
-
-    this.globalCompositeOperation = 'source-over';
-
-    this.strokeStyle = 'rgb(0, 0, 0)';
-    this.beginPath();
-    this.moveTo($scope.last.x, $scope.last.y);
-    this.lineTo(point.x, point.y);
-    this.stroke();
-
-    $scope.last = point;
-  }
-
-  function initialDraw($scope, ctx) {
-    ctx.clearRect(0, 0, $scope.width, $scope.height);
-
-    if($scope.grid) {
-      drawGrid.call(ctx, 0, $scope.width, $scope.height);
-    }
-
-    // draw 1V helper
-    ctx.lineWidth = 1.5;
-    ctx.strokeStyle = 'rgb(0, 0, 0)';
-    ctx.beginPath();
-    ctx.moveTo(5,  translateY($scope.height, 0));
-    ctx.lineTo(10, translateY($scope.height, 0));
-    ctx.lineTo(10, translateY($scope.height, 1));
-    ctx.lineTo(30, translateY($scope.height, 1));
-    ctx.lineTo(30, translateY($scope.height, 0));
-    ctx.lineTo(35, translateY($scope.height, 0));
-    ctx.stroke();
-    ctx.lineWidth = 1;
-  }
-
   return {
     restrict: 'E',
     template: '<canvas width="{{width}}" height="{{height}}"></canvas>',
@@ -148,42 +193,26 @@ angular
     },
 
     link: function($scope, element, attrs) {
-      if(!$scope.width)  { $scope.width = 800; }
-      if(!$scope.height) { $scope.height = Settings.ppm * 30 + margin.top + margin.bottom; }
       if($scope.noise === undefined) { $scope.noise = 0; }
+      if(!$scope.width) { $scope.width = 800; }
+      if(!$scope.height) {
+        $scope.height = Settings.ppm * 30 +
+                        ECGSimulator.prototype.MARGIN.top +
+                        ECGSimulator.prototype.MARGIN.bottom;
+      }
 
       $scope.static = attrs.static !== undefined;
       $scope.grid   = attrs.noGrid === undefined;
 
-      var ctx = element.find('canvas')[0].getContext('2d');
+      var ctx       = element.find('canvas')[0].getContext('2d'),
+          simulator = new ECGSimulator(Presets, Settings, ResultWave, ctx);
 
-      $scope.last = {
-        offset: 0,
-        x: margin.left,
-        y: translateY($scope.height, ResultWave(0, $scope.preset || 'normal', $scope.noise))
-      };
+      simulator.reset($scope);
+      setTimeout(simulator.run.bind(simulator, $scope));
 
       $scope.$on('reset-ecg-simulation', function() {
-        $scope.last = {
-          offset: 0,
-          x: margin.left,
-          y: translateY($scope.height, ResultWave(0, $scope.preset || 'normal', $scope.noise))
-        };
-
-        initialDraw($scope, ctx);
-      });
-
-      // should run on after canvas had been set
-      setTimeout(function() {
-        initialDraw($scope, ctx);
-
-        window.requestAnimationFrame(function handler() {
-          drawHandler.call(ctx, $scope);
-
-          if(!$scope.shouldStopAnimation) {
-            window.requestAnimationFrame(handler);
-          }
-        });
+        simulator.clear($scope);
+        simulator.reset($scope);
       });
     }
   };
